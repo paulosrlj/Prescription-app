64f0f63d4dbc8a21769f31ed6603c7c5d8e472e3
paulosrlj <paulo.leite@academico.ifpb.edu.br>
2021-09-25 21:11:12 -0300

    :wrench: fix - Eslint and prettier configs

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,24 @@
+export default {
+  colors: {
+    primary: '#4ACCC3',
+
+    secondary_blue: '#49DAD0',
+
+    success: '#12A454',
+    success_light: 'rgba(18, 164, 84, 0.5)',
+
+    attention: '#DA3030',
+    attention_light: 'rgba(218, 48, 48, 0.5)',
+
+    title: '#000000',
+    text: '#000000',
+    info: '#7E7676',
+    background: '#FFFFFF',
+  },
+
+  fonts: {
+    regular: 'Poppins_400Regular',
+    medium: 'Poppins_500Medium',
+    bold: 'Poppins_700Bold',
+  },
+};
