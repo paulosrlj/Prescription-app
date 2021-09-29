df2e752ed1b432447ff73d9e9c2e344f6b577839
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2021-09-29 17:49:26 -0300

    :sparkles: feat - create screen login

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -1,5 +1,14 @@
 export default {
   colors: {
+    attention: '#DA3030',
+    attention_light: 'rgba(218, 48, 48, 0.5)',
+
+    background: '#FFFFFF',
+    background_input: '#A5959514',
+
+    info: '#7E7676',
+
+    paragraph: '#7E7676',
     primary: '#4ACCC3',
 
     secondary_blue: '#49DAD0',
@@ -7,18 +16,14 @@ export default {
     success: '#12A454',
     success_light: 'rgba(18, 164, 84, 0.5)',
 
-    attention: '#DA3030',
-    attention_light: 'rgba(218, 48, 48, 0.5)',
-
     title: '#000000',
     text: '#FFFFFF',
     text_black: '#000000',
-    info: '#7E7676',
-    background: '#FFFFFF',
   },
 
   fonts: {
     regular: 'Quicksand_400Regular',
+    medium: 'Quicksand_500Medium',
     bold: 'Quicksand_700Bold',
   },
 };
