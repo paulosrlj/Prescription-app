3a5945f145670110c4de8654f2f531bfbf426a12
José Ricardo <jricardobgoncalves@gmail.com>
2021-09-30 19:06:39 -0300

    Autenticação nas rotas

---
+++ src/routes/authenticated.routes.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useTheme } from 'styled-components';
+
+import { MaterialCommunityIcons, SimpleLineIcons } from '@expo/vector-icons';
+import Settings from '../screens/Settings';
+import HomePaciente from '../screens/HomePaciente';
+
+const { Navigator, Screen } = createBottomTabNavigator();
+
+export default function AuthenticatedRoutes(): JSX.Element {
+  const theme = useTheme();
+
+  return (
+    <Navigator
+      screenOptions={{
+        tabBarActiveTintColor: theme.colors.primary,
+        tabBarInactiveTintColor: theme.colors.info,
+        headerShown: false,
+        tabBarLabel: () => null,
+      }}
+    >
+      <Screen
+        name="HomePaciente"
+        component={HomePaciente}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <SimpleLineIcons name="credit-card" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Screen
+        name="SearchMedicine"
+        component={HomePaciente} // TODO -> trocar quando a tela tiver pronta
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialCommunityIcons name="pill" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Screen
+        name="SearchDrugstores"
+        component={HomePaciente}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <SimpleLineIcons name="location-pin" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Screen
+        name="Settings"
+        component={Settings}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <SimpleLineIcons name="menu" size={size} color={color} />
+          ),
+        }}
+      />
+    </Navigator>
+  );
+}
