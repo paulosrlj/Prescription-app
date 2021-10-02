d8df624d2ad0271d2de66a73bc84980cd83730b6
luluthedevil <luciana.miranda@academico.ifpb.edu.br>
2021-10-02 20:13:58 -0300

    procurar medicamentos

--- src/components/MedicineCard/styles.ts
+++ src/components/MedicineCard/styles.ts
@@ -5,7 +5,7 @@ import { RFValue } from 'react-native-responsive-fontsize';
 import { RecipeText } from '../MedicineItem/style';
 
 export const Container = styled.TouchableHighlight`
-  height: 165px;
+  height: 185px;
   width: 100%;
 
   border: 1px solid ${({ theme }) => theme.colors.secondary_blue};
@@ -24,17 +24,24 @@ export const Icon = styled(Entypo)`
 `;
 
 export const TextContainer = styled.View`
-  margin-left: 5px;
+  margin-left: 10px;
 `;
 
 export const Title = styled.Text`
   font-family: ${({ theme }) => theme.fonts.regular};
-  font-size: 25px;
+  font-size: 27px;
   color: ${({ theme }) => theme.colors.text_black};
 `;
 
 export const Label = styled(RecipeText)`
   font-family: ${({ theme }) => theme.fonts.regular};
+  margin-top: 5px;
+`;
+
+export const Negrito = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.bold};
+  font-size: 15px;
+  color: ${({ theme }) => theme.colors.text_black};
 `;
 
 export const Vality = styled.Text`
